3e971484f98d31740ca1e5b3a916540d5a2f7596
Merlin Siimsalu <msiimsal@tktk.ee>
2021-12-05 15:49:04 +0200

    add new projects

---
+++ MyShoppingList/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace DirectoriesAndFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string rootDirectory = @"C:\Users\Kasutaja\samples";
+            Console.WriteLine("Enter directory name: ");
+            string newDirectory = Console.ReadLine();
+            Console.WriteLine("Enter file name: ");
+            string fileName = Console.ReadLine();
+
+
+
+            if (Directory.Exists($"{rootDirectory}\\{newDirectory}") && File.Exists($"{rootDirectory}\\{newDirectory}\\{fileName}"))
+            {
+                Console.WriteLine($"Directory and File exist.");
+            }
+            else
+            {
+                Directory.CreateDirectory($"{rootDirectory}\\{newDirectory}");
+                File.Create($"{rootDirectory}\\{newDirectory}\\{fileName}");
+            }
+
+
+            string[] arrayFromFile = File.ReadAllLines($"{newDirectory}{fileName}");
+            List<string> myWishList = arrayFromFile.ToList<string>();
+
+            bool loopActive = true;
+
+            while (loopActive)
+            {
+                Console.WriteLine("Would you like to add a wish? Y/N: ");
+                char userInput = Convert.ToChar(Console.ReadLine().ToLower());
+
+                if (userInput == 'y')
+                {
+                    Console.WriteLine("Enter your wich: ");
+                    string userWish = Console.ReadLine();
+                    myWishList.Add(userWish);
+                }
+                else
+                {
+                    loopActive = false;
+                    Console.WriteLine("Take care!");
+                }
+
+                Console.Clear();
+
+                foreach (string wish in myWishList)
+                {
+                    Console.WriteLine($"Your wish: {wish}.");
+                }
+
+                File.WriteAllLines($"{newDirectory}{fileName}", myWishList);
+            }
+        }
+    }
+}
